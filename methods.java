536de97d27bb9ac4b49b3f5824f42af645bbadc1
STANLEY LEO <stanleyitan95@gmail.com>
2021-03-23 23:01:56 +0100

    first commit

---
+++ methods.java
@@ -0,0 +1,13 @@
+package com.stanlee.eitan.leo;
+public class TestMethods{
+    public static int sum(int i1,int i2){
+    int result = 0;
+    for (i >= i1 ;i <= i2; i++)
+      result += i ;
+      return result;}
+      public static void main(String[] args){
+          system.out.println("sum from 10 to 20 is; " +sum(10,20));
+          system.out.println("sum from 20 to 30 is; " +sum(20,30));
+          system.out.println("sum from 30 to 40 is; " +sum(30,40));
+      }
+}
